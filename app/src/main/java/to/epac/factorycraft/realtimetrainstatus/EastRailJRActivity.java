e77e5b2b9aefd918c44d903d67032e13c48906a2
i998979 <brianx2x@gmail.com>
2026-02-03 20:42:56 +0800

    Update

--- app/src/main/java/to/epac/factorycraft/realtimetrainstatus/EastRailJRActivity.java
+++ app/src/main/java/to/epac/factorycraft/realtimetrainstatus/EastRailJRActivity.java
@@ -4,12 +4,15 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.util.Log;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -21,6 +24,7 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class EastRailJRActivity extends AppCompatActivity {
@@ -30,8 +34,14 @@ public class EastRailJRActivity extends AppCompatActivity {
     private List<Trip> activeTrips = new ArrayList<>();
     private final Handler mainHandler = new Handler(Looper.getMainLooper());
 
+    private String lineCode = "eal";
     // Low Wu -> Admiralty, no Racecourse
-    private final int[] EAL_STATION_CODES = {13, 12, 11, 10, 9, 8, 6, 5, 4, 3, 2, 21, 22, 23};
+    private int[] stationCodes = {13, 12, 11, 10, 9, 8, 6, 5, 4, 3, 2, 21, 22, 23};
+
+    private HashMap<Integer, Long> runTimeUpMap = new HashMap<>();
+    private HashMap<Integer, Long> runTimeDnMap = new HashMap<>();
+    private HashMap<Integer, Long> dwellTimeUpMap = new HashMap<>();
+    private HashMap<Integer, Long> dwellTimeDnMap = new HashMap<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,16 +49,12 @@ public class EastRailJRActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_east_rail_jr);
 
-        androidx.core.view.WindowInsetsControllerCompat controller =
-                new androidx.core.view.WindowInsetsControllerCompat(getWindow(), getWindow().getDecorView());
+        WindowInsetsControllerCompat controller = new WindowInsetsControllerCompat(getWindow(), getWindow().getDecorView());
 
-        // 檢查目前是否為夜間模式
         boolean isDarkMode = (getResources().getConfiguration().uiMode &
                 android.content.res.Configuration.UI_MODE_NIGHT_MASK) ==
                 android.content.res.Configuration.UI_MODE_NIGHT_YES;
 
-        // 如果是日間模式，將圖示設為深色 (!isDarkMode = true)
-        // 如果是夜間模式，將圖示設為淺色 (!isDarkMode = false)
         controller.setAppearanceLightStatusBars(!isDarkMode);
         controller.setAppearanceLightNavigationBars(!isDarkMode);
 
@@ -61,11 +67,111 @@ public class EastRailJRActivity extends AppCompatActivity {
             return insets;
         });
 
-        adapter = new JRLineAdapter(this, EAL_STATION_CODES, activeTrips);
+        runTimeUpMap = new HashMap<>();
+        runTimeDnMap = new HashMap<>();
+        dwellTimeUpMap = new HashMap<>();
+        dwellTimeDnMap = new HashMap<>();
+
+        // --- 北行 (Up: ADM -> LOW/LMC) ---
+        // Run Time (行車)
+        runTimeUpMap.put(1, 94L);
+        runTimeUpMap.put(2, 185L);
+        runTimeUpMap.put(3, 211L);
+        runTimeUpMap.put(4, 123L);
+        runTimeUpMap.put(5, 216L);
+        runTimeUpMap.put(6, 99L);
+        runTimeUpMap.put(7, 120L);
+        runTimeUpMap.put(8, 163L);
+        runTimeUpMap.put(9, 303L);
+        runTimeUpMap.put(10, 97L);
+        runTimeUpMap.put(11, 265L);
+        runTimeUpMap.put(12, 106L);
+        runTimeUpMap.put(13, 202L);
+        runTimeUpMap.put(14, 408L);
+
+        // Dwell Time (停站)
+        dwellTimeUpMap.put(23, 47L);
+        dwellTimeUpMap.put(1, 44L);
+        dwellTimeUpMap.put(2, 44L);
+        dwellTimeUpMap.put(3, 47L);
+        dwellTimeUpMap.put(4, 47L);
+        dwellTimeUpMap.put(5, 35L);
+        dwellTimeUpMap.put(6, 35L);
+        dwellTimeUpMap.put(7, 35L);
+        dwellTimeUpMap.put(8, 35L);
+        dwellTimeUpMap.put(9, 35L);
+        dwellTimeUpMap.put(10, 35L);
+        dwellTimeUpMap.put(11, 35L);
+        dwellTimeUpMap.put(12, 47L);
+
+        // --- 南行 (Dn: LOW/LMC -> ADM) ---
+        // Run Time (行車)
+        runTimeDnMap.put(12, 197L); // 預設羅湖出發
+        runTimeDnMap.put(11, 106L);
+        runTimeDnMap.put(10, 262L);
+        runTimeDnMap.put(9, 96L);
+        runTimeDnMap.put(8, 301L);
+        runTimeDnMap.put(6, 160L);
+        runTimeDnMap.put(7, 120L);
+        runTimeDnMap.put(5, 98L);
+        runTimeDnMap.put(4, 220L);
+        runTimeDnMap.put(3, 125L);
+        runTimeDnMap.put(2, 158L);
+        runTimeDnMap.put(1, 184L);
+        runTimeDnMap.put(23, 93L);
+
+        // Dwell Time (停站)
+        dwellTimeDnMap.put(13, 47L);
+        dwellTimeDnMap.put(14, 47L);
+        dwellTimeDnMap.put(12, 35L);
+        dwellTimeDnMap.put(11, 35L);
+        dwellTimeDnMap.put(10, 40L);
+        dwellTimeDnMap.put(9, 30L);
+        dwellTimeDnMap.put(8, 35L);
+        dwellTimeDnMap.put(6, 40L);
+        dwellTimeDnMap.put(7, 40L);
+        dwellTimeDnMap.put(5, 47L);
+        dwellTimeDnMap.put(4, 47L);
+        dwellTimeDnMap.put(3, 44L);
+        dwellTimeDnMap.put(2, 44L);
+        dwellTimeDnMap.put(1, 44L);
+
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            lineCode = extras.getString("LINE_CODE", "eal");
+            int[] codes = extras.getIntArray("STATION_CODES");
+            if (codes != null) stationCodes = codes;
+
+            if (extras.getSerializable("RUN_TIME_UP_MAP") != null)
+                runTimeUpMap = (HashMap<Integer, Long>) extras.getSerializable("RUN_TIME_UP_MAP");
+            if (extras.getSerializable("RUN_TIME_DN_MAP") != null)
+                runTimeDnMap = (HashMap<Integer, Long>) extras.getSerializable("RUN_TIME_DN_MAP");
+
+            if (extras.getSerializable("DWELL_TIME_UP_MAP") != null)
+                dwellTimeUpMap = (HashMap<Integer, Long>) extras.getSerializable("DWELL_TIME_UP_MAP");
+            if (extras.getSerializable("DWELL_TIME_DN_MAP") != null)
+                dwellTimeDnMap = (HashMap<Integer, Long>) extras.getSerializable("DWELL_TIME_DN_MAP");
+
+        }
+        /*lineCode = "tml";
+        stationCodes = new int[]{49, 48, 47, 46, 45, 44, 43, 42, 41, 50, 14, 1,
+                61, 62, 63, 64, 65, 66, 21, 22, 23, 24, 25, 26, 27, 28, 29};*/
+
+        adapter = new JRLineAdapter(this, lineCode, stationCodes, activeTrips);
         rvEastRailLine.setAdapter(adapter);
 
         rvEastRailLine.setClipChildren(false);
         rvEastRailLine.setClipToPadding(false);
+
+        TextView tvBannerName = findViewById(R.id.tv_banner_name);
+        tvBannerName.setText(Utils.getLineName(lineCode, true));
+
+        FrameLayout lineBanner = findViewById(R.id.line_banner);
+        int colorResId = getResources().getIdentifier(this.lineCode.toLowerCase(), "color", getPackageName());
+        int lineColor = getResources().getColor(colorResId, null);
+        lineBanner.setBackgroundColor(lineColor);
+
 
         startRefreshLoop();
     }
