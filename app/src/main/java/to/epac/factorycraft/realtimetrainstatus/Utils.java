377304d1345abf947a49491dc4331a06d044b4ee
i998979 <brianx2x@gmail.com>
2026-02-06 00:20:39 +0800

    Update

--- app/src/main/java/to/epac/factorycraft/realtimetrainstatus/Utils.java
+++ app/src/main/java/to/epac/factorycraft/realtimetrainstatus/Utils.java
@@ -1,6 +1,7 @@
 package to.epac.factorycraft.realtimetrainstatus;
 
 import android.content.Context;
+import android.util.Log;
 
 import androidx.core.content.ContextCompat;
 
@@ -14,221 +15,40 @@ import java.util.Map;
 
 public class Utils {
 
-    public static boolean isInteger(String str) {
+    public static String idToCode(Context context, int code, String id) {
         try {
-            Integer.parseInt(str);
-            return true;
-        } catch (NumberFormatException e) {
-            return false;
-        }
-    }
+            int idRes = context.getResources().getIdentifier(id.toLowerCase() + "_station_id", "string", context.getPackageName());
+            int codeRes = context.getResources().getIdentifier(id.toLowerCase() + "_station_code", "string", context.getPackageName());
 
-    public static String mapStation(int code, String line) {
-        // TML
-        if (line.equalsIgnoreCase("TML")) {
-            switch (code) {
-                case 1:
-                    return "HUH";
-                case 14:
-                    return "ETS";
-                case 21:
-                    return "TAW";
-                case 22:
-                    return "CKT";
-                case 23:
-                    return "STW";
-                case 24:
-                    return "CIO";
-                case 25:
-                    return "SHM";
-                case 26:
-                    return "TSH";
-                case 27:
-                    return "HEO";
-                case 28:
-                    return "MOS";
-                case 29:
-                    return "WKS";
-                case 41:
-                    return "NAC";
-                case 42:
-                    return "MEF";
-                case 43:
-                    return "TWW";
-                case 44:
-                    return "KSR";
-                case 45:
-                    return "YUL";
-                case 46:
-                    return "LOP";
-                case 47:
-                    return "TIS";
-                case 48:
-                    return "SIH";
-                case 49:
-                    return "TUM";
-                case 50:
-                    return "AUS";
-                case 61:
-                    return "HOM";
-                case 62:
-                    return "TKW";
-                case 63:
-                    return "SUW";
-                case 64:
-                    return "KAT";
-                case 65:
-                    return "DIH";
-                case 66:
-                    return "HIK";
-                case 91:
-                    return "DEP";
-                case 92:
-                    return "OUT";
-                case 93:
-                    return "SPC";
-                case 94:
-                    return "TES";
-                case 95:
-                    return "WR";
-                case 97:
-                    return "MOL";
-                default:
-                    return String.valueOf(code);
-            }
-        }
-        // EAL
-        else if (line.equalsIgnoreCase("EAL")) {
-            switch (code) {
-                case 0:
-                    return "HTD";
-                case 15:
-                    return "SSG_S";
-                case 151:
-                    return "SSG_N";
-                case 16:
-                    return "LWM_S";
-                case 161:
-                    return "LWM_N";
-                case 17:
-                    return "S1";
-                case 701:
-                    return "HTD";
-                case 901:
-                    return "HTD_N";
-                case 24:
-                    return "ADT";
-                case 241:
-                    return "ADT_1";
+            String[] ids = context.getString(idRes).split("\\s+");
+            String[] codes = context.getString(codeRes).split("\\s+");
 
-                case 1:
-                    return "HUH_1";
-                case 2:
-                    return "MKK";
-                case 3:
-                    return "KOT";
-                case 4:
-                    return "TAW";
-                case 5:
-                    return "SHT";
-                case 6:
-                    return "FOT";
-                case 7:
-                    return "RAC";
-                case 8:
-                    return "UNI";
-                case 9:
-                    return "TAP";
-                case 10:
-                    return "TWO";
-                case 11:
-                    return "FAN";
-                case 12:
-                    return "SHS";
-                case 13:
-                    return "LOW";
-                case 14:
-                    return "LMC";
-                case 21:
-                    return "HUH";
-                case 22:
-                    return "EXC";
-                case 23:
-                    return "ADM";
-                case 91:
-                    return "HTD";
-                case 92:
-                    return "HTD_1";
-                case 81:
-                    return "TEST";
-                case 82:
-                    return "TEST_1";
-                default:
-                    return String.valueOf(code);
+            for (int i = 0; i < ids.length; i++) {
+                if (Integer.parseInt(ids[i]) == code) {
+                    return codes[i];
+                }
             }
+        } catch (Exception e) {
         }
         return String.valueOf(code);
     }
 
-    public static int covertStationOrder(int code) {
-        switch (code) {
-            case 1:
-                return 16;
-            case 14:
-                return 17;
-            case 21:
-                return 9;
-            case 22:
-                return 8;
-            case 23:
-                return 7;
-            case 24:
-                return 6;
-            case 25:
-                return 5;
-            case 26:
-                return 4;
-            case 27:
-                return 3;
-            case 28:
-                return 2;
-            case 29:
-                return 1;
-            case 41:
-                return 19;
-            case 42:
-                return 20;
-            case 43:
-                return 21;
-            case 44:
-                return 22;
-            case 45:
-                return 23;
-            case 46:
-                return 24;
-            case 47:
-                return 25;
-            case 48:
-                return 26;
-            case 49:
-                return 27;
-            case 50:
-                return 18;
-            case 61:
-                return 15;
-            case 62:
-                return 14;
-            case 63:
-                return 13;
-            case 64:
-                return 12;
-            case 65:
-                return 11;
-            case 66:
-                return 10;
-            default:
-                return 0;
+    public static int codeToId(Context context, String line, String code) {
+        try {
+            int idRes = context.getResources().getIdentifier(line.toLowerCase() + "_station_id", "string", context.getPackageName());
+            int codeRes = context.getResources().getIdentifier(line.toLowerCase() + "_station_code", "string", context.getPackageName());
+
+            String[] ids = context.getString(idRes).split("\\s+");
+            String[] codes = context.getString(codeRes).split("\\s+");
+
+            for (int i = 0; i < codes.length; i++) {
+                if (codes[i].equalsIgnoreCase(code)) {
+                    return Integer.parseInt(ids[i]);
+                }
+            }
+        } catch (Exception e) {
         }
+        return -1;
     }
 
     public static String getStationName(Context context, String code) {
@@ -295,6 +115,84 @@ public class Utils {
 
         int code = context.getResources().getIdentifier(line.toLowerCase(), "color", context.getPackageName());
         return "#" + Integer.toHexString(ContextCompat.getColor(context, code));
+    }
+
+    public static long convertTimestampToMillis(String timeStr) {
+        if (timeStr == null || timeStr.isEmpty()) return 0;
+        try {
+            // Open Data 格式: "2026-02-04 23:51:02"
+            java.text.SimpleDateFormat sdf = new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss", java.util.Locale.getDefault());
+
+            // 關鍵：強制指定為 GMT+8 (香港時間)
+            sdf.setTimeZone(java.util.TimeZone.getTimeZone("GMT+8"));
+
+            java.util.Date date = sdf.parse(timeStr);
+            return date != null ? date.getTime() : 0;
+        } catch (Exception e) {
+            Log.e("Utils", "Time parse error: " + timeStr);
+            return 0;
+        }
+    }
+
+    public static int covertStationOrder(int code) {
+        switch (code) {
+            case 1:
+                return 16;
+            case 14:
+                return 17;
+            case 21:
+                return 9;
+            case 22:
+                return 8;
+            case 23:
+                return 7;
+            case 24:
+                return 6;
+            case 25:
+                return 5;
+            case 26:
+                return 4;
+            case 27:
+                return 3;
+            case 28:
+                return 2;
+            case 29:
+                return 1;
+            case 41:
+                return 19;
+            case 42:
+                return 20;
+            case 43:
+                return 21;
+            case 44:
+                return 22;
+            case 45:
+                return 23;
+            case 46:
+                return 24;
+            case 47:
+                return 25;
+            case 48:
+                return 26;
+            case 49:
+                return 27;
+            case 50:
+                return 18;
+            case 61:
+                return 15;
+            case 62:
+                return 14;
+            case 63:
+                return 13;
+            case 64:
+                return 12;
+            case 65:
+                return 11;
+            case 66:
+                return 10;
+            default:
+                return 0;
+        }
     }
 
     public static List<LatLng> getLatLngs(String s) {
