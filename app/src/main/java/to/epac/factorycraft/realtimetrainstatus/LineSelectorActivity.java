f77661ac02c7be123b32b71fccc86e7178ced0f7
i998979 <brianx2x@gmail.com>
2026-02-09 23:39:04 +0800

    Update

--- app/src/main/java/to/epac/factorycraft/realtimetrainstatus/LineSelectorActivity.java
+++ app/src/main/java/to/epac/factorycraft/realtimetrainstatus/LineSelectorActivity.java
@@ -23,10 +23,16 @@ public class LineSelectorActivity extends AppCompatActivity {
         Switch openData = findViewById(R.id.openData);
 
         List<LineItem> lines = new ArrayList<>();
-        // 添加東鐵綫
-        lines.add(new LineItem("EAL", "東鐵綫", "#5DE2FF"));
-        // 添加屯馬綫
-        lines.add(new LineItem("TML", "屯馬綫", "#9A3848"));
+        lines.add(new LineItem("EAL", "東鐵綫", "#5eb7e8"));
+        lines.add(new LineItem("TML", "屯馬綫", "#9c2e00"));
+        lines.add(new LineItem("KTL", "觀塘綫", "#00a040"));
+        lines.add(new LineItem("AEL", "機場快綫", "#00888e"));
+        lines.add(new LineItem("DRL", "迪士尼綫", "#eb6ea5"));
+        lines.add(new LineItem("ISL", "港島綫", "#0075c2"));
+        lines.add(new LineItem("TCL", "東涌綫", "#f3982d"));
+        lines.add(new LineItem("TKL", "將軍澳綫", "#7e3c93"));
+        lines.add(new LineItem("TWL", "荃灣綫", "#e60012"));
+        lines.add(new LineItem("SIL", "南港島綫", "#cbd300"));
 
         LineSelectorAdapter adapter = new LineSelectorAdapter(lines, line -> {
             Intent intent = new Intent(this, EastRailJRActivity.class);
