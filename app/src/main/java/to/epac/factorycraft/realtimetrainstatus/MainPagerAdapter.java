d526818d7eabb5526ceb8bdb90502e0de732d794
i998979 <brianx2x@gmail.com>
2026-02-12 05:39:42 +0800

    Update

--- app/src/main/java/to/epac/factorycraft/realtimetrainstatus/MainPagerAdapter.java
+++ app/src/main/java/to/epac/factorycraft/realtimetrainstatus/MainPagerAdapter.java
@@ -2,25 +2,22 @@ package to.epac.factorycraft.realtimetrainstatus;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentActivity;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 
 public class MainPagerAdapter extends FragmentStateAdapter {
-    public MainPagerAdapter(@NonNull FragmentActivity fragmentActivity) {
-        super(fragmentActivity);
+    public MainPagerAdapter(@NonNull Fragment fragment) {
+        super(fragment);
     }
 
     @NonNull
     @Override
     public Fragment createFragment(int position) {
         switch (position) {
-            case 0: // Route Search
-                return new RouteSearchFragment();
-            case 1: // Location Map
+            case 0: // Location Map
                 return WebViewFragment.newInstance("https://www.mtr.com.hk/archive/ch/services/layouts/adm.pdf");
-            case 2: // Street Map
+            case 1: // Street Map
                 return WebViewFragment.newInstance("https://www.mtr.com.hk/archive/ch/services/maps/hok.pdf");
-            case 3: // Realtime Train Location
+            case 2: // Realtime Train Location
                 return new LineSelectorFragment();
             default:
                 return new Fragment();
@@ -29,6 +26,6 @@ public class MainPagerAdapter extends FragmentStateAdapter {
 
     @Override
     public int getItemCount() {
-        return 4;
+        return 3;
     }
 }
