8715a7c59dc830cd05d86b7cddeb22708b3398e9
i998979 <brianx2x@gmail.com>
2026-02-13 20:45:49 +0800

    Update

--- app/src/main/java/to/epac/factorycraft/realtimetrainstatus/AppDatabase.java
+++ app/src/main/java/to/epac/factorycraft/realtimetrainstatus/AppDatabase.java
@@ -3,7 +3,7 @@ package to.epac.factorycraft.realtimetrainstatus;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
-@Database(entities = {SearchHistory.class}, version = 1)
+@Database(entities = {SearchHistory.class, StationHistory.class}, version = 1)
 public abstract class AppDatabase extends RoomDatabase {
     public abstract SearchHistoryDao searchHistoryDao();
 }
