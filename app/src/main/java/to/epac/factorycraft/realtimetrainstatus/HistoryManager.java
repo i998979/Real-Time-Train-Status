a7454f13f5b514e1bde5d967dded88b8bb6da9c0
i998979 <brianx2x@gmail.com>
2026-02-13 22:48:48 +0800

    Update

--- app/src/main/java/to/epac/factorycraft/realtimetrainstatus/HistoryManager.java
+++ app/src/main/java/to/epac/factorycraft/realtimetrainstatus/HistoryManager.java
@@ -11,33 +11,66 @@ import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
 public class HistoryManager {
+    private static HistoryManager instance;
+
     private final AppDatabase db;
     private final Executor executor = Executors.newSingleThreadExecutor();
+    private final Handler handler = new Handler(Looper.getMainLooper());
 
-    public HistoryManager(Context context) {
+    private HistoryManager(Context context) {
         db = Room.databaseBuilder(context.getApplicationContext(),
-                AppDatabase.class, "search_history_db").build();
+                        AppDatabase.class, "search_history_db")
+                // Old version database migration
+                .fallbackToDestructiveMigrationFrom(1)
+                .fallbackToDestructiveMigration()
+                .build();
     }
 
-    public void saveSearch(String oId, String dId, String oName, String dName) {
+    public static synchronized HistoryManager getInstance(Context context) {
+        if (instance == null)
+            instance = new HistoryManager(context);
+
+        return instance;
+    }
+
+    public void saveRouteSearch(String oId, String dId, String oName, String dName) {
         executor.execute(() -> {
             SearchHistoryDao dao = db.searchHistoryDao();
-            // 1. 插入新記錄
-            dao.insert(new SearchHistory(oId, dId, oName, dName));
-            // 2. 清理舊記錄
-            dao.deleteOldHistories();
+            dao.insertRoutes(new SearchHistory(oId, dId, oName, dName));
         });
     }
 
-    public void loadHistory(OnHistoryLoadedListener listener) {
+    public void loadSearchHistory(OnHistoryLoadedListener listener) {
         executor.execute(() -> {
-            List<SearchHistory> history = db.searchHistoryDao().getRecentHistories();
-            // 將結果切換回主執行緒以更新 UI
-            new Handler(Looper.getMainLooper()).post(() -> listener.onLoaded(history));
+            List<SearchHistory> history = db.searchHistoryDao().getRecentRoutes();
+            handler.post(() -> {
+                listener.onLoaded(history);
+            });
+        });
+    }
+
+    public void saveStationSearch(int stationId, String stationName) {
+        executor.execute(() -> {
+            SearchHistoryDao dao = db.searchHistoryDao();
+            dao.insertStation(new StationHistory(stationId, stationName));
+
+        });
+    }
+
+    public void loadStationHistory(OnStationHistoryLoadedListener listener) {
+        executor.execute(() -> {
+            List<StationHistory> history = db.searchHistoryDao().getRecentStations();
+            handler.post(() -> {
+                listener.onLoaded(history);
+            });
         });
     }
 
     public interface OnHistoryLoadedListener {
         void onLoaded(List<SearchHistory> history);
+    }
+
+    public interface OnStationHistoryLoadedListener {
+        void onLoaded(List<StationHistory> history);
     }
 }
