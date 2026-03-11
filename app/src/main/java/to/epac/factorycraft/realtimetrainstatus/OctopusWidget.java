c4121ec51da71a43417702c1d38342fa0db85f7f
i998979 <brianx2x@gmail.com>
2026-03-11 20:19:10 +0800

    Update

--- app/src/main/java/to/epac/factorycraft/realtimetrainstatus/OctopusWidget.java
+++ app/src/main/java/to/epac/factorycraft/realtimetrainstatus/OctopusWidget.java
@@ -11,22 +11,20 @@ import android.widget.RemoteViews;
 
 public class OctopusWidget extends AppWidgetProvider {
 
-    public static final String OCTOPUS_PACKAGE = "com.octopuscards.nfc_reader";
-
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         for (int appWidgetId : appWidgetIds) {
             RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_octopus);
 
             PackageManager pm = context.getPackageManager();
-            Intent intent = pm.getLaunchIntentForPackage(OCTOPUS_PACKAGE);
+            Intent intent = pm.getLaunchIntentForPackage(MainActivity.OCTOPUS_PACKAGE);
 
             if (intent != null) {
                 PendingIntent pendingIntent = PendingIntent.getActivity(
                         context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
                 views.setOnClickPendingIntent(R.id.widget_container, pendingIntent);
             } else {
-                Intent marketIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + OCTOPUS_PACKAGE));
+                Intent marketIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + MainActivity.OCTOPUS_PACKAGE));
                 PendingIntent pendingIntent = PendingIntent.getActivity(
                         context, 0, marketIntent, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
                 views.setOnClickPendingIntent(R.id.widget_container, pendingIntent);
