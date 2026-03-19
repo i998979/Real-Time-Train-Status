452e9cc28f89c1ec42d66fb640de3818dcea6c12
i998979 <brianx2x@gmail.com>
2026-03-19 20:46:14 +0800

    Update

--- app/src/androidTest/java/to/epac/factorycraft/transitapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/to/epac/factorycraft/transitapp/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("to.epac.factorycraft.realtimetrainstatus", appContext.getPackageName());
+        assertEquals("to.epac.factorycraft.transitapp", appContext.getPackageName());
     }
 }
