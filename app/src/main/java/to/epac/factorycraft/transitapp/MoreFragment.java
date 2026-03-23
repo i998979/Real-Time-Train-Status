8c8ad04a35c67b5bec904e06cbab287f886b302e
i998979 <brianx2x@gmail.com>
2026-03-23 22:49:31 +0800

    Update

--- app/src/main/java/to/epac/factorycraft/transitapp/MoreFragment.java
+++ app/src/main/java/to/epac/factorycraft/transitapp/MoreFragment.java
@@ -3,6 +3,7 @@ package to.epac.factorycraft.transitapp;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -26,6 +27,7 @@ public class MoreFragment extends Fragment {
     private MaterialCardView btnOctopus;
     private MaterialCardView btnMtrMobile;
     private LinearLayout mediaContainer;
+    private GridLayout gridContainer;
 
     @Nullable
     @Override
@@ -39,10 +41,20 @@ public class MoreFragment extends Fragment {
         btnMtrMobile = view.findViewById(R.id.btn_mtrmobile);
         btnMtrMobile.setOnClickListener(v -> openExternalApp(MainActivity.MTRMOBILE_PACKAGE));
         mediaContainer = view.findViewById(R.id.media_container);
+        gridContainer = view.findViewById(R.id.grid_menu_container);
 
 
         int[] images = {R.drawable.a, R.drawable.b, R.drawable.c, R.drawable.d, R.drawable.e};
         String[] titles = {"尖東路軌改道工程", "現代化列車退役", "CHiiKAWA DAYS", "站見鐵路展", "市區綫復古列車"};
+        String[] imgUrls = {
+                "https://www.instagram.com/footsteps_33.6km/p/DVqx5oLEzGx/",
+                "https://www.instagram.com/footsteps_33.6km/p/DTzxP-tEa-S/",
+                "https://www.instagram.com/footsteps_33.6km/p/DOGLzrVE6CE/",
+                "https://www.instagram.com/footsteps_33.6km/p/DFHcVvHT_ME/",
+                "https://www.instagram.com/footsteps_33.6km/p/DB0gdbDhSPF/"
+        };
+
+        int targetWidthPx = Utils.dpToPx(requireContext(), 140);
 
         for (int i = 0; i < images.length; i++) {
             View card = inflater.inflate(R.layout.item_jr_media_card, mediaContainer, false);
@@ -51,32 +63,36 @@ public class MoreFragment extends Fragment {
             TextView tvRank = card.findViewById(R.id.tv_media_rank);
             TextView tvTitle = card.findViewById(R.id.tv_media_title);
 
-            ivMedia.setImageResource(images[i]);
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inJustDecodeBounds = true;
+            BitmapFactory.decodeResource(getResources(), images[i], options);
+
+            int inSampleSize = 1;
+            if (options.outWidth > targetWidthPx)
+                inSampleSize = Math.round((float) options.outWidth / (float) targetWidthPx);
+
+            options.inSampleSize = inSampleSize;
+            options.inJustDecodeBounds = false;
+
+            ivMedia.setImageBitmap(BitmapFactory.decodeResource(getResources(), images[i], options));
+
             tvRank.setText(String.valueOf(i + 1));
             tvTitle.setText(titles[i]);
 
+            String targetUrl = imgUrls[i];
             card.setOnClickListener(v -> {
-                // Card click listener
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(targetUrl));
+                startActivity(intent);
             });
 
-            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
-                    Utils.dpToPx(requireContext(), 140),
-                    ViewGroup.LayoutParams.WRAP_CONTENT
-            );
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(targetWidthPx, ViewGroup.LayoutParams.WRAP_CONTENT);
 
-            if (i < images.length - 1) {
-                params.rightMargin = Utils.dpToPx(requireContext(), 12);
-            } else {
-                params.rightMargin = 0;
-            }
+            params.rightMargin = i < images.length - 1 ? Utils.dpToPx(requireContext(), 12) : 0;
 
             card.setLayoutParams(params);
-
             mediaContainer.addView(card);
         }
 
-
-        GridLayout gridContainer = view.findViewById(R.id.grid_menu_container);
 
         String[] labels = {"鐵流 Railic HK", "鐵流 Railic HK", "footsteps_33.6km", "footsteps_33.6km"};
         int[] icons = {R.drawable.railic, R.drawable.youtube, R.drawable.footsteps_33_6km, R.drawable.youtube};
